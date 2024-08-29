aca6dfce014d6cc5ace2cea43bc0e64e6c4018fc
Dan Toledano <dan3tol99@gmail.com>
2024-08-29 11:40:20 +0300

    security (xss+ queryInjection + dos)

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorControllers');
@@ -11,10 +14,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //1) Global middlewares
+//Set security HTTP headers
+app.use(helmet());
+
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000, // 60 minutes
@@ -22,7 +30,14 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body object
+app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization  against NoSQL query injection
+app.use(mongoSanitize());
+//Data sanitization against XSS
+app.use(xss());
+// Serving static files from the public directory (usually for images, CSS, JS, etc.)
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
--- utils/email.js
+++ utils/email.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
