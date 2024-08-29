931e761003c648f23881052a9298dbb12015597f
Dan Toledano <dan3tol99@gmail.com>
2024-08-29 12:09:00 +0300

    hpp

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorControllers');
@@ -35,8 +36,24 @@ app.use(express.json({ limit: '10kb' }));
 
 //Data sanitization  against NoSQL query injection
 app.use(mongoSanitize());
+
 //Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+      'ratingsAverage',
+      'ratingsQuantity',
+    ], // allow listed params
+  }),
+);
+
 // Serving static files from the public directory (usually for images, CSS, JS, etc.)
 app.use(express.static(`${__dirname}/public`));
 
